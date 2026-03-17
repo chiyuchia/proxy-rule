d168bd2ce7be897c2add789f49761adbb0a64d4e
chiyuchia <chiyuchia@outlook.com>
2026-03-17 20:46:30 +0800

    feat(proxy-groups): 在代理组中新增 DIRECT 选项

--- config_overwrite.js
+++ config_overwrite.js
@@ -2,9 +2,7 @@ function main(config) {
   const originalProxyGroups = config?.['proxy-groups'] || [];
   config['proxy-groups'] = originalProxyGroups.map((group) => {
     if (!group.filter && !group.name.includes('全球直连')) {
-      console.log('🚀 ~ main ~ group.filter:', group.name);
       group.proxies = (group?.proxies ?? []).concat(config.proxies.map((proxy) => proxy.name));
-      console.log('🚀 ~ main ~ group.proxies:', group.proxies);
     }
     return group;
   });
